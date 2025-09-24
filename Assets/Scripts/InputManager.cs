30477b3df2a487fdb45860019018ad3b6dcfaa49
Lizbeth <lizgricas@gmail.com>
2025-09-24 18:08:08 -0500

    Player input management
    Player scripts are configured to organize, centralize, and control key aspects of the player's input system, animations, and movements.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerControls playerControls;
+    AnimatorManager animatorManager;
+    PlayerMovement playerMovement;
+
+    public Vector2 movementInput;
+
+    public float verticalInput;
+    public float horizontalInput;
+    public float moveAmount;
+
+    public bool shiftInput;
+
+    private void Awake()
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+            playerControls.Player.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+
+            playerControls.ActionsMap.Shift.performed += i => shiftInput = true;
+            playerControls.ActionsMap.Shift.canceled += i => shiftInput = false;
+        }
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+
+        animatorManager.UpdateAnimatorValue(0, moveAmount, playerMovement.isRunning);
+    }
+
+    public void HandleAllInputs ()
+    {
+        HandleMovementInput();
+        HandleRunningInput();
+    }
+
+    private void HandleRunningInput()
+    {
+        if (shiftInput && moveAmount > 0.5f)
+            playerMovement.isRunning = true;
+        else
+            playerMovement.isRunning = false;
+    }
+}
